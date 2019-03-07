288f96bf1b686ff11259bd13d98a99619db05ff4
Lucy Ivatt <li571@its.york.ac.uk>
2019-03-07 12:58:02 +0000

    Added project files

---
+++ src/Bar.java
@@ -0,0 +1,23 @@
+
+public class Bar {
+	private int left, right, height;
+	
+	public Bar(int left, int right, int height) {
+		this.left = left;
+		this.right = right;
+		this.height = height;
+	}
+
+	public int getLeft() {
+		return left;
+	}
+
+	public int getRight() {
+		return right;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+}
